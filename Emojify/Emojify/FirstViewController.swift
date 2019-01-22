e2dbd525b30bb895652bac61cc8d5e0d9641e5c7
Imad ENNEIYMY <imenneiymy@a106mac11.local>
2019-01-22 14:11:25 +0100

    Project Init commit

---
+++ Emojify/Emojify/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Emojify
+//
+//  Created by Imad ENNEIYMY on 22/01/2019.
+//  Copyright © 2019 Imad ENNEIYMY. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Emojify/Emojify/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Emojify
+//
+//  Created by Imad ENNEIYMY on 22/01/2019.
+//  Copyright © 2019 Imad ENNEIYMY. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
